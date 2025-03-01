af7579247ecbcf8b8d584c63c880812726873f9b
notAvoiid <igor.nacimento51@gmail.com>
2025-03-01 19:40:25 -0300

    feat: add initial structure

---
+++ src/main/java/com/abreu/download_link/DownloadLinkApplication.java
@@ -0,0 +1,13 @@
+package com.abreu.download_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abreu/download_link/DownloadLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abreu.download_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
